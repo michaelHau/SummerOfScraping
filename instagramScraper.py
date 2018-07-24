79262fe030a7fa1d23fd39dd7ae8f06967011719
Michael D Hau <pker1066@gmail.com>
2018-07-23 21:11:50 -0700

    completed base function

--- instagramScraper.py
+++ instagramScraper.py
@@ -6,15 +6,22 @@ import random
 from bs4 import BeautifulSoup
 from selenium import webdriver 
 
-url = 'https://www.instagram.com/p/BlWycAUHHR5/?taken-by=teasocietyofficial'
+url = input("Enter the URL of an Instagram post, then press ENTER")
+#https://www.instagram.com/p/BlWycAUHHR5/?taken-by=teasocietyofficial
+
+deviceName = input("What is the name of your device (your user directory)?")
 
 option = webdriver.ChromeOptions()
 option.add_argument(" - incognito")
-
 browser = webdriver.Chrome(executable_path='/Users/michaelhau/Library/Application Support/Google/chromedriver', chrome_options=option)
 browser.get(url)
 
 buttonElement = browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/article/div[2]/div[1]/ul/li[2]/a')
+browser.execute_script("arguments[0].scrollIntoView()", buttonElement)
+time.sleep(5)
+browser.execute_script("window.scrollBy(0,-50)", "")
+buttonElement.click()
+
 browser.execute_script("arguments[0].scrollIntoView()", buttonElement)
 time.sleep(5)
 browser.execute_script("window.scrollBy(0,-50)", "")
@@ -30,12 +37,13 @@ for nameLi in bodySoup.findAll('li', attrs={'class':'gElp9'}):
 	if username != "teasocietyofficial": #removes post owner from input
 		listOfNames.append(username)
 
-filteredListOfNames = []
+filteredListOfNames = [] #removes duplicate names
 
 for name in listOfNames:
 	if name not in filteredListOfNames:
 		filteredListOfNames.append(name)
 
-print(listOfNames[random.randint(0,len(filteredListOfNames))])
+print(filteredListOfNames[random.randint(0,len(filteredListOfNames))])
+
 
 
